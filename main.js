74af163130a78e07b93dc831d2aaed55f76f8c90
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-14 14:47:48 -0600

    2, 3:49

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hi");
+// Colour Array
+
+// variables
+let containerEl = document.getElementById("container");
+
+// Array
+let colours = ["red", "green", "blue", "orange", "cyan"];
+
+// Click Event
+document.addEventListener("click", update);
+
+function update() {
+    containerEl.innerHTML = `<div style="background: ${colours[randomInt(0, 5)]}"></div>`;
+}
+
+
+
